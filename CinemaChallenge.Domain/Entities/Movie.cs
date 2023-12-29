79a120dd90e90651f8885466e9c64ca11553c5da
Luan Bianchini <luanbianchine1@gmail.com>
2023-12-29 11:16:33 -0300

    feat: create movie entity

---
+++ CinemaChallenge.Domain/Entities/Movie.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaChallenge.Domain.Entities
+{
+    internal class Movie
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Synopsis { get; set; }
+        public string? ReleaseDate { get; set; }
+
+        private Movie(string title, string synopsis, string? releaseDate)
+        {
+            Id = Guid.NewGuid();
+            Title = title;
+            Synopsis = synopsis;
+            ReleaseDate = releaseDate ?? "Em breve";
+        }
+
+        public static Movie Create (string title, string synopsis, string? releaseDate)
+        {
+            return new Movie(title, synopsis, releaseDate);
+        }
+    }
+}
