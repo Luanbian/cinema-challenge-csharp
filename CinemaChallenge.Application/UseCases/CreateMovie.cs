5086bd0e5b9d9d0d452e8df8429315736c89c787
Luan Bianchini <luanbianchine1@gmail.com>
2023-12-30 10:55:39 -0300

    feat: create usecase to create movie

--- CinemaChallenge.Application/UseCases/CreateMovie.cs
+++ CinemaChallenge.Application/UseCases/CreateMovie.cs
@@ -1,21 +1,20 @@
 ﻿using CinemaChallenge.Application.Interfaces;
 using CinemaChallenge.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using CinemaChallenge.Infra.Data.Interfaces;
 
 namespace CinemaChallenge.Application.UseCases
 {
-    public class CreateMovie : ICreateMovie
+    public class CreateMovie(ICreateRepository<Movie> create) : ICreateMovie
     {
+        private readonly ICreateRepository<Movie> repository = create;
         public Movie Perform()
         {
             string title = "Clube da luta";
             string synopsis = "Não falamos sobre o clube da luta";
             string releaseDate = "15/10/1999";
-            return Movie.Create(title, synopsis, releaseDate);
+            Movie movie = Movie.Create(title, synopsis, releaseDate);
+            repository.Create(movie);
+            return movie;
         }
     }
 }
