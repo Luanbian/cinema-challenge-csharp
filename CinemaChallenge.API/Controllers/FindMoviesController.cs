b5c2e6c5f0c9b6a26e038e1c3bb1b2c5e0eadd55
Luan Bianchini <luanbianchine1@gmail.com>
2023-12-30 22:58:12 -0300

    feat: create controller to list movies

---
+++ CinemaChallenge.API/Controllers/FindMoviesController.cs
@@ -0,0 +1,27 @@
+﻿using CinemaChallenge.Application.Interfaces;
+using CinemaChallenge.Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaChallenge.API.Controllers
+{
+    [Route("api/movies")]
+    [ApiController]
+    public class FindMoviesController(IFindMovie find) : ControllerBase
+    {
+        private readonly IFindMovie find = find;
+
+        [HttpGet]
+        public IActionResult Handle()
+        {
+            try
+            {
+                List<Movie> movies = find.Perform();
+                return Ok(movies);
+            } catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
