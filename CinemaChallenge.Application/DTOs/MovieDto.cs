b541422181d08d0ed3623fb2aa0d9b8902e4dfdc
Luan Bianchini <luanbianchine1@gmail.com>
2023-12-31 09:54:16 -0300

    feat: create movie dto

---
+++ CinemaChallenge.Application/DTOs/MovieDto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CinemaChallenge.Application.DTOs
+{
+    public record MovieDto
+    {
+        [Required(ErrorMessage = "Título é obrigatório")]
+        public string Title { get; init; }
+
+        [Required(ErrorMessage = "Sinopse é obrigatória")]
+        public string Synopsis { get; init; }
+
+        [Required(ErrorMessage = "Data de lançamento é obrigatória")]
+        public string ReleaseDate { get; init; }
+
+        public MovieDto(string title, string synopsis, string releaseDate)
+        {
+            Title = title;
+            Synopsis = synopsis;
+            ReleaseDate = releaseDate;
+        }
+    }
+}
