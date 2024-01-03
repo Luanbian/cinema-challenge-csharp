1340c3f369e5bb2841bfea70b8cdcbab11c25f56
Luan Bianchini <luanbianchine1@gmail.com>
2024-01-03 15:36:33 -0300

    feat: implements IsDeleted and deletedAt in entity

--- CinemaChallenge.Domain/Entities/Movie.cs
+++ CinemaChallenge.Domain/Entities/Movie.cs
@@ -3,9 +3,12 @@
     public class Movie
     {
         public Guid Id { get; set; }
+        public DateTime? CreatedAt { get; set; }
         public string Title { get; set; }
         public string Synopsis { get; set; }
         public string? ReleaseDate { get; set; }
+        public bool? IsDeleted { get; set; }
+        public DateTime? DeletedAt { get; set; }
 
         private Movie(string title, string synopsis, string? releaseDate)
         {
@@ -13,6 +16,9 @@
             Title = title;
             Synopsis = synopsis;
             ReleaseDate = releaseDate ?? "Em breve";
+            CreatedAt = DateTime.Now;
+            IsDeleted = false;
+            DeletedAt = null;
         }
 
         public static Movie Create(string title, string synopsis, string? releaseDate)
