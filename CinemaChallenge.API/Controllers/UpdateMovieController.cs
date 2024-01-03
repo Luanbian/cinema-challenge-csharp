a10e4a72559e1848cf93dd26372881b42199ed32
Luan Bianchini <luanbianchine1@gmail.com>
2024-01-03 12:10:08 -0300

    feat: create controller to update movie in patch

---
+++ CinemaChallenge.API/Controllers/UpdateMovieController.cs
@@ -0,0 +1,25 @@
+﻿using CinemaChallenge.Application.Interfaces;
+using CinemaChallenge.Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaChallenge.API.Controllers
+{
+    [Route("api/movies")]
+    [ApiController]
+    public class UpdateMovieController(IUpdateMovie update) : ControllerBase
+    {
+        private readonly IUpdateMovie update = update;
+        [HttpPatch]
+        public IActionResult Handle([FromQuery] string id, [FromBody] IMovie data)
+        {
+            try
+            {
+                update.Perform(id, data);
+                return Ok($"atualizado o item {id}");
+            } catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
