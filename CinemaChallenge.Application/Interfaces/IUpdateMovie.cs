66d611b2332dba85b6d83e46e02589ee7cf1dafd
Luan Bianchini <luanbianchine1@gmail.com>
2024-01-03 12:04:57 -0300

    feat: create usecase to update movie

---
+++ CinemaChallenge.Application/Interfaces/IUpdateMovie.cs
@@ -0,0 +1,10 @@
+﻿using CinemaChallenge.Domain.Entities;
+using CinemaChallenge.Domain.Interfaces;
+
+namespace CinemaChallenge.Application.Interfaces
+{
+    public interface IUpdateMovie
+    {
+        void Perform(string id, IMovie data);
+    }
+}
---
+++ CinemaChallenge.Application/UseCases/UpdateMovie.cs
@@ -0,0 +1,15 @@
+﻿using CinemaChallenge.Application.Interfaces;
+using CinemaChallenge.Domain.Interfaces;
+using CinemaChallenge.Infra.Data.Interfaces;
+
+namespace CinemaChallenge.Application.UseCases
+{
+    public class UpdateMovie(IUpdateRepository<IMovie> update) : IUpdateMovie
+    {
+        private readonly IUpdateRepository<IMovie> repository = update;
+        public void Perform(string id, IMovie data)
+        {
+            repository.Update(new Guid(id), data);
+        }
+    }
+}
