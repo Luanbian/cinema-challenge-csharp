7b9e0d43f3f691375ea0c040af33259e8355b472
Luan Bianchini <luanbianchine1@gmail.com>
2024-01-03 15:40:11 -0300

    feat: migratio sto apply more columns in db

---
+++ CinemaChallenge.Infra.Data/Migrations/20240103183925_UpdateMoviesColumns.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CinemaChallenge.Infra.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateMoviesColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Movies",
+                type: "datetime(6)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "Movies",
+                type: "datetime(6)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Movies",
+                type: "tinyint(1)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Movies");
+        }
+    }
+}
