6fc2665d5828826fb37b7a7c85d7fee008ea1515
Luan Bianchini <luanbianchine1@gmail.com>
2024-01-04 16:51:42 -0300

    refact: apply diff in program.cs

--- CinemaChallenge.API/Program.cs
+++ CinemaChallenge.API/Program.cs
@@ -1,5 +1,8 @@
+using CinemaChallenge.Application.DTOs;
 using CinemaChallenge.Application.Interfaces;
+using CinemaChallenge.Application.UseCases;
 using CinemaChallenge.Application.UseCases.Movies;
+using CinemaChallenge.Application.UseCases.Users;
 using CinemaChallenge.Domain.Entities;
 using CinemaChallenge.Domain.Interfaces;
 using CinemaChallenge.Infra.Data.EntityFramework;
@@ -15,10 +18,12 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 //usecases
-builder.Services.AddScoped<ICreateMovie, CreateMovie>();
-builder.Services.AddScoped<IFindMovie, FindMovie>();
-builder.Services.AddScoped<IUpdateMovie, UpdateMovie>();
-builder.Services.AddScoped<IDeleteMovie, DeleteMovie>();
+builder.Services.AddScoped<ICreate<Movie, MovieDto>, CreateMovie>();
+builder.Services.AddScoped<ICreate<User, UserDto>, CreateUser>();
+builder.Services.AddScoped<IFind<Movie, IMovie>, FindMovie>();
+builder.Services.AddScoped<IUpdate<IMovie>, UpdateMovie>();
+builder.Services.AddScoped<IUpdate<MovieDto>, UpdateMovie>();
+builder.Services.AddScoped<IDelete, DeleteMovie>();
 
 //repositories
 builder.Services.AddScoped<ICreateRepository<Movie>, EFCreateMovie>();
