23a9568cbed27ab6a388356646134e07dd4b975a
Luan Bianchini <luanbianchine1@gmail.com>
2024-01-04 16:08:23 -0300

    feat: add create user repository

---
+++ CinemaChallenge.Infra.Data/Repositories/EFCreateUser.cs
@@ -0,0 +1,17 @@
+﻿using CinemaChallenge.Domain.Entities;
+using CinemaChallenge.Infra.Data.EntityFramework;
+using CinemaChallenge.Infra.Data.Interfaces;
+
+namespace CinemaChallenge.Infra.Data.Repositories
+{
+    public class EFCreateUser(AppDbContext appDbContext) : ICreateRepository<User>
+    {
+        private readonly AppDbContext db = appDbContext;
+
+        public void Create(User data)
+        {
+            db.Users.Add(data);
+            db.SaveChanges();
+        }
+    }
+}
