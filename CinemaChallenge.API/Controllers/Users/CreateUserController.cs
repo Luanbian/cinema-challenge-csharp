c187a2f9d846597a8eb30f1a0a67cefafee82a02
Luan Bianchini <luanbianchine1@gmail.com>
2024-01-04 16:44:17 -0300

    feat: add controller to create user

---
+++ CinemaChallenge.API/Controllers/Users/CreateUserController.cs
@@ -0,0 +1,28 @@
+﻿using CinemaChallenge.Application.DTOs;
+using CinemaChallenge.Application.Interfaces;
+using CinemaChallenge.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaChallenge.API.Controllers.Users
+{
+    [Route("api/users")]
+    [ApiController]
+    public class CreateUserController(ICreate<User, UserDto> create) : ControllerBase
+    {
+        private readonly ICreate<User, UserDto> create = create;
+
+        [HttpPost]
+        public IActionResult Handle([FromBody] UserDto userDto)
+        {
+            try
+            {
+                if (!ModelState.IsValid) return BadRequest(ModelState);
+                User user = create.Perform(userDto);
+                return Ok(user);
+            } catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
