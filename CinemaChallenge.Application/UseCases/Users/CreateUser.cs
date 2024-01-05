3517518595beae3caed3c9f0c728346c20fbc35f
Luan Bianchini <luanbianchine1@gmail.com>
2024-01-05 15:46:30 -0300

    feat: apply encrypter in usecase

--- CinemaChallenge.Application/UseCases/Users/CreateUser.cs
+++ CinemaChallenge.Application/UseCases/Users/CreateUser.cs
@@ -5,14 +5,15 @@ using CinemaChallenge.Infra.Data.Interfaces;
 
 namespace CinemaChallenge.Application.UseCases.Users
 {
-    public class CreateUser(ICreateRepository<User> create) : ICreate<User, UserDto>
+    public class CreateUser(ICreateRepository<User> create, IEncrypter encrypter) : ICreate<User, UserDto>
     {
         private readonly ICreateRepository<User> repository = create;
+        private readonly IEncrypter encrypter = encrypter;
         public User Perform(UserDto data)
         {
             string Name = data.Name;
             string Email = data.Email;
-            string Password = data.Password;
+            string Password = encrypter.Encrypt(data.Password);
             User user = User.Create(Name, Email, Password);
             repository.Create(user);
             return user;
