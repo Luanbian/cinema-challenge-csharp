02b6a92d29193a19a5d897588b82d058953c888c
Luan Bianchini <luanbianchine1@gmail.com>
2024-01-05 16:53:42 -0300

    feat: add loginDto and LoginController

---
+++ CinemaChallenge.API/Controllers/Users/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using CinemaChallenge.Application.DTOs;
+using CinemaChallenge.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaChallenge.API.Controllers.Users
+{
+    [Route("api/auth/login")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Handle([FromBody] LoginDto loginDto)
+        {
+            try
+            {
+                if (!ModelState.IsValid) return BadRequest(ModelState);
+                string token = await Login.Perform(loginDto);
+                return Ok(token);
+            } catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
---
+++ CinemaChallenge.Application/DTOs/LoginDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CinemaChallenge.Application.DTOs
+{
+    public record LoginDto(string Email, string Password)
+    {
+        [Required(ErrorMessage = "Email é obrigatório")]
+        public string Email { get; init; } = Email;
+
+        [Required(ErrorMessage = "Senha é obrigatória")]
+        public string Password { get; init; } = Password;
+    }
+}
